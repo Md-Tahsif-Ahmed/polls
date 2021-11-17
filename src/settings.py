d33bd21727b51b0277aab18eef608651eec9c77b
Md-Tahsif_Ahmed <tahsif.cse@gmail.com>
2021-11-18 00:27:45 +0600

    Profile changed

--- src/settings.py
+++ src/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'src.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [r'D:\Django\ElectionPolls\core\templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
