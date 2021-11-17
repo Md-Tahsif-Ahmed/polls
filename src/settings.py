7fbf16004c80187dfbc9ae85677ff2998e2399a8
Md-Tahsif_Ahmed <tahsif.cse@gmail.com>
2021-11-17 23:40:54 +0600

    Host commit

--- src/settings.py
+++ src/settings.py
@@ -128,5 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIR= [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATIC_STORAGE='whitenoise.storage.CompressedManyfestStaticFilesStorage'
 django_heroku.settings(locals())
