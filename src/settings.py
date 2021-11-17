9f11b48e7996a47c05d6c8b6fc10f282ee73f047
Md-Tahsif_Ahmed <tahsif.cse@gmail.com>
2021-11-17 14:01:06 +0600

    Host commit

--- src/settings.py
+++ src/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import os
+
 from pathlib import Path
+import os
+import django_heroku
+import dj_database_url
+from decouple import config
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,3 +128,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
